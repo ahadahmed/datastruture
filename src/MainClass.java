11b84871e2711da28d1c3467a17e7d3d7fee3cc1
ahad <ahadahmed.career@gmail.com>
2019-03-05 13:17:03 +0600

    Initial commit.

---
+++ src/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Admin
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/ds/linear/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Admin
+ *
+ */
+package ds.linear;
